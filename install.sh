bccb94ba8a99e4266de4171a4758df8a12053e62
Marc Pope <marcpope@me.com>
2022-10-19 08:38:28 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 apt -y install ffmpeg
 pip install setuptools-rust
 pip install git+https://github.com/openai/whisper.git 
+wget https://media.truthcdn.com/shows/374/49946.mp3
+whisper 49946.mp3 --model base.en
